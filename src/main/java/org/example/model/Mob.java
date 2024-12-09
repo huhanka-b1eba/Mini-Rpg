2d453438672f97198fc0afbe3094c130390c330f
Aigiz Tulybaev <flaylay256@gmail.com>
2024-12-09 22:51:49 +0300

    Mob created

--- src/main/java/org/example/model/Mob.java
+++ src/main/java/org/example/model/Mob.java
@@ -1,4 +1,22 @@
 package org.example.model;
 
 public class Mob {
+    protected String name;
+
+    public Mob(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Привет " + name;
+    }
 }
